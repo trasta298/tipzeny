69a9438f1c8c20279fbb2b684f750547456d7d26
trasta <i.taiki720@gmail.com>
2017-12-18 13:35:27 +0900

    rain実装
    その他細かい表記修正

--- tipzeny.py
+++ tipzeny.py
@@ -2,6 +2,7 @@
 #-*- coding:utf-8 -*-
 
 from decimal import *
+import json
 import re
 import logging
 import sqlite3
@@ -97,6 +98,23 @@ def on_tweet(status):
 			gettip(status.user.id)
 			balance = zeny.getbalance(account,6)
 			all_balance = zeny.getbalance(account,0)
+
+			if all_balance < 10:
+				f = open('accountlist.json','r')
+				json_data = json.load(f)
+				if status.user.id in json_data:
+					datas = filter(lambda x: x == status.user.id, json_data)
+					w = open('accountlist.json','w')
+					json.dump(datas, w)
+			else:
+				f = open('accountlist.json','r')
+				json_data = json.load(f)
+				if status.user.id not in json_data:
+					json_data.append(status.user.id)
+					w = open('accountlist.json','w')
+					json.dump(json_data, w)
+
+
 			logger.info("check balance..." + name)
 			logger.info(DecimaltoStr(balance) + "ZNY all(" + DecimaltoStr(all_balance) + "ZNY)")
 			tweet = "@" + name + " " + DecimaltoStr(balance) + u"ZNY持ってます！"
@@ -111,6 +129,57 @@ def on_tweet(status):
 			tweet = "@" + name + " " + address + u" に送金お願いしますっ！"
 			api.update_status(status=tweet, in_reply_to_status_id=status.id)
 
+		elif re.match("withdrawall", message) or re.match(u"全額出金", message):
+			m = re.split(" ", message)
+			if len(m) < 2:
+				helptweet(status, u"withdrawall(全額出金)の使い方\n@￰zenytips withdrawall 受取ZNYアドレス\nhttps://github.com/trasta298/tipzeny/wiki")
+				return
+
+			address = m[1]
+			balance = zeny.getbalance(account,6)
+			tax = 0.01
+			payying = float(balance)-tax
+
+			logger.info("withdraw..."+name)
+
+			if round(0-payying,7) >= 0:
+				logger.info("-> Not enough ZNY (0.01 > " + str(payying) + ")")
+				tweet = "@" + name + u" 残高が足りないみたいですっ！\n所持zny: " + str(balance) + "ZNY"
+				api.update_status(status=tweet, in_reply_to_status_id=status.id)
+				return
+
+			validate = zeny.validateaddress(address)
+			if not validate['isvalid']:
+				logger.info("-> Invalid address")
+				tweet = "@" + name + u" アドレスが間違ってるみたいですっ"
+				api.update_status(status=tweet, in_reply_to_status_id=status.id)
+				return
+
+			logger.info("-> Sending...")
+			txid = zeny.sendfrom(account,address,payying)
+
+			# taxまわりとりあえず後回し
+			logger.info("-> Checking transaction...")
+			tx = zeny.gettransaction(txid)
+			if tx:
+				fee = float(tx['fee'])
+				logger.info("-> Tax: " + str(fee))
+			else:
+				fee = 0
+				logger.info("-> No Tax")
+
+			zeny.move(account, "taxpot", tax+fee)
+			logger.info("-> Fee sent to taxpot: " + str(tax+fee) + "ZNY")
+			tweet = "@" + name + u" zenyを引き出しましたっ！(手数料0.01ZNY)\nhttp://namuyan.dip.jp/MultiLightBlockExplorer/gettxid.php?coin=zeny&txid=" + str(txid)
+			api.update_status(status=tweet, in_reply_to_status_id=status.id)
+
+			f = open('accountlist.json','r')
+			json_data = json.load(f)
+			if status.user.id in json_data:
+				datas = filter(lambda x: x == status.user.id, json_data)
+				w = open('accountlist.json','w')
+				json.dump(datas, w)
+
 		elif re.match("withdraw", message) or re.match(u"出金", message):
 			m = re.split(" ", message)
 			if len(m) < 3 or not str_isfloat(m[2]):
@@ -118,8 +187,10 @@ def on_tweet(status):
 				return
 
 			address = m[1]
-			amount = float(m[2])
+			amot= float(m[2])
 			balance = zeny.getbalance(account,6)
+			tax = 0.01
+			amount = amot-tax
 
 			if amount <= 0:
 				tweet = "@" + name + u" 0以下の数は指定できません！"
@@ -128,7 +199,7 @@ def on_tweet(status):
 
 			logger.info("withdraw..."+name)
 
-			if round(Decimal(amount)-balance,7) > 0:
+			if round(Decimal(amount)-balance,7) >= 0:
 				logger.info("-> Not enough ZNY ("+ DecimaltoStr(balance) + " < " + str(amount) + ")")
 				tweet = "@" + name + u" 残高が足りないみたいですっ！\n所持zny: " + DecimaltoStr(balance) + "ZNY"
 				api.update_status(status=tweet, in_reply_to_status_id=status.id)
@@ -148,51 +219,60 @@ def on_tweet(status):
 			logger.info("-> Checking transaction...")
 			tx = zeny.gettransaction(txid)
 			if tx:
-				fee = tx['fee']
-				#logger.info("-> Tax: " + str(fee))
+				fee = float(tx['fee'])
+				logger.info("-> Tax: " + str(fee))
 			else:
 				fee = 0
-				#logger.info("-> No Tax")
+				logger.info("-> No Tax")
 
-			#zeny.move(account, "taxpot", fee)
-			#logger.info("-> Fee sent to taxpot: " + str(fee) + "ZNY")
-			tweet = "@" + name + u" zenyを引き出しましたっ！\nhttp://namuyan.dip.jp/MultiLightBlockExplorer/gettxid.php?coin=zeny&txid=" + str(txid)
+			zeny.move(account, "taxpot", tax+fee)
+			logger.info("-> Fee sent to taxpot: " + str(tax+fee) + "ZNY")
+			tweet = "@" + name + u" zenyを引き出しましたっ！(手数料0.01ZNY)\nhttp://namuyan.dip.jp/MultiLightBlockExplorer/gettxid.php?coin=zeny&txid=" + str(txid)
 			api.update_status(status=tweet, in_reply_to_status_id=status.id)
 
-		elif re.match("withdrawall", message) or re.match(u"全額出金", message):
-			m = re.split(" ", message)
-			if len(m) < 3 or not str_isfloat(m[2]):
-				helptweet(status, u"withdraw(出金)の使い方\n@￰zenytips withdraw 受取ZNYアドレス 出金額(ZNY)\nhttps://github.com/trasta298/tipzeny/wiki")
-				return
+			if float(balance)-amount < 10:
+				f = open('accountlist.json','r')
+				json_data = json.load(f)
+				if status.user.id in json_data:
+					datas = filter(lambda x: x == status.user.id, json_data)
+					w = open('accountlist.json','w')
+					json.dump(datas, w)
 
-			address = m[1]
+		elif re.match("rain", message) or re.match(u"撒き銭", message):
+			m = re.split(" ", message)
+			if len(m) < 2 or not str_isfloat(m[1]):
+				helptweet(status, u"rain(撒き銭)の使い方\n@￰zenytips rain 撒銭額(ZNY)\nhttps://github.com/trasta298/tipzeny/wiki")
+				return
+			amount = float(m[1])
 			balance = zeny.getbalance(account,6)
 
-			logger.info("withdraw..."+name)
-
-			validate = zeny.validateaddress(address)
-			if not validate['isvalid']:
-				logger.info("-> Invalid address")
-				tweet = "@" + name + u" アドレスが間違ってるみたいですっ"
+			if amount <= 0:
+				tweet = "@" + name + u" 0以下の数は指定できません！"
 				api.update_status(status=tweet, in_reply_to_status_id=status.id)
 				return
 
-			logger.info("-> Sending...")
-			txid = zeny.sendfrom(account,address,balance)
+			logger.info("Raining..."+str(amount)+"ZNY from "+name)
 
-			# taxまわりとりあえず後回し
-			logger.info("-> Checking transaction...")
-			tx = zeny.gettransaction(txid)
-			if tx:
-				fee = tx['fee']
-				#logger.info("-> Tax: " + str(fee))
-			else:
-				fee = 0
-				#logger.info("-> No Tax")
+			if round(Decimal(amount)-balance,7) > 0:
+				logger.info("-> Not enough ZNY ("+ DecimaltoStr(balance) + " < " + str(amount) + ")")
+				tweet = "@" + name + u" 残高が足りないみたいですっ！\n所持zny: " + DecimaltoStr(balance) + "ZNY"
+				api.update_status(status=tweet, in_reply_to_status_id=status.id)
+				return
 
-			#zeny.move(account, "taxpot", fee)
-			#logger.info("-> Fee sent to taxpot: " + str(fee) + "ZNY")
-			tweet = "@" + name + u" zenyを引き出しましたっ！\nhttp://namuyan.dip.jp/MultiLightBlockExplorer/gettxid.php?coin=zeny&txid=" + str(txid)
+			f = open('accountlist.json','r')
+			json_data = json.load(f)
+			nmb = len(json_data)
+			if nmb == 0:
+				tweet = "@" + name + u" 対象者がいないみたいです。。。"
+				api.update_status(status=tweet, in_reply_to_status_id=status.id)
+
+			mon = round(amount/nmb,7)
+			for userid in json_data:
+				to_account = "tipzeny-" + str(userid)
+				zeny.move(account,to_account,mon)
+			logger.info("-> Sent.")
+
+			tweet = "@" + name + u" " + str(nmb) + u"人にそれぞれ " + str(mon) + u"ZNYずつ送りましたっ！"
 			api.update_status(status=tweet, in_reply_to_status_id=status.id)
 
 		elif re.match("send", message) or re.match(u"送金", message):
@@ -278,7 +358,7 @@ def on_tweet(status):
 			tweet = "@" + to + u" りん姫より @" + name + u" さんから" + str(amount) + u"ZNYのお届け物だよっ！ 3日以内にbalanceして受け取ってね！"
 			api.update_status(status=tweet, in_reply_to_status_id=status.id)
 
-		else:
+		elif len(message) < 2:
 			helptweet(status, u"りん姫の使い方はここっ\nhttps://github.com/trasta298/tipzeny/wiki")
 
 
